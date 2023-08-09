78e59fcf6e626fb7862dc97e12208394048979af
Shovon Islam <101002417+Shovonx47@users.noreply.github.com>
2023-08-08 19:09:50 -0700

    Newsfeed first commit

---
+++ ajax.js
@@ -0,0 +1,90 @@
+console.log("Ajax tutorial in one video");
+
+
+let fetchBtn = document.getElementById('fetchBtn');
+fetchBtn.addEventListener('click', buttonClickHandler)
+
+function buttonClickHandler() {
+     console.log('You have clicked the fetchBtn');
+
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+
+    // USE THIS FOR POST REQUEST
+    xhr.open('POST', 'https://jsonplaceholder.typicode.com/todos/1', true);
+    xhr.getResponseHeader('Content-type', 'application/json');
+
+
+    // What to do on progress (optional)
+    xhr.onprogress = function(){
+        console.log('On progress');
+    }
+
+
+    // xhr.onreadystatechange = function () {
+    //     console.log('ready state is ', xhr.readyState);
+        
+    // }
+
+    // What to do when response is ready
+    xhr.onload = function () {
+        if(this.status === 200){
+
+            console.log(this.responseText)
+        }
+        else{
+            console.log("Some error occured")
+        }
+    }
+
+    // send the request
+    params = `{"userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false}`;
+    xhr.send(params);
+
+    console.log("We are done!");
+
+}
+
+let popBtn = document.getElementById('popBtn');
+popBtn.addEventListener('click', popHandler);
+
+function popHandler() {
+    
+
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+
+
+    // What to do when response is ready
+    xhr.onload = function () {
+        if(this.status === 200){
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str = "";
+            for (key in obj)
+            {
+                str += `<li>${obj[key].employee_name} </li>`;
+            }
+            list.innerHTML = str;
+        }
+        else{
+            console.log("Some error occured")
+        }
+    }
+
+    // send the request
+    xhr.send();
+    
+    
+}
+

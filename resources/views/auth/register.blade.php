ecfb18161059fb745616bbb5a95d0e158b411afd
DaifulFardous <wsmoasis2@gmail.com>
2023-08-26 23:31:44 +0600

    Home page fix , user login/sign up fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -61,4 +61,103 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>REGISTRATION</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" type="image/png" href="{{ asset('login') }}/images/icons/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('login') }}/css/util.css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('login') }}/css/main.css" />
+    <link
+      href="{{ asset('login') }}/css/font-awesome/css/all.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
+      integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="limiter">
+      <div
+        class="container-login100"
+        style="background-image: url('{{ asset('login') }}/images/image-18.jpg')"
+      >
+        <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+          <form class="login100-form validate-form" action="{{ route('register') }}" method="post">
+            @csrf
+            <span class="login100-form-title p-b-49"> Registration </span>
+            <div class="wrap-input100">
+                <input
+                  class="input100"
+                  type="text"
+                  name="name" :value="old('name')" required autofocus
+                  placeholder="Username:"
+                />
+              </div>
+            <div class="wrap-input100" style="margin-top: 5px">
+              <input
+                class="input100"
+                type="email"
+                name="email" :value="old('email')" required
+                placeholder="Email:"
+              />
+            </div>
+            <div class="wrap-input100" style="margin-top: 5px">
+              <input
+                class="input100"
+                type="phone"
+                name="phone" required
+                placeholder="Phone:"
+              />
+            </div>
+            <div class="wrap-input100 m-t-5">
+              <input
+                class="input100"
+                type="password"
+                name="password"
+                :value="__('Password')"
+                placeholder="Password:"
+              />
+            </div>
+            <div class="wrap-input100 m-t-5">
+              <input
+                class="input100"
+                type="password"
+                name="password_confirmation" required
+                placeholder="Confirm Password:"
+              />
+            </div>
+            <div class="container-login100-form-btn" style="margin-top: 10px">
+              <div class="wrap-login100-form-btn">
+                <div class="login100-form-bgbtn"></div>
+                <button class="login100-form-btn" type="submit">Sign Up</button>
+              </div>
+            </div>
+            <div style="margin-top: 10px">
+              <span>Find us on</span>
+            </div>
+            <i class="fab fa-facebook"></i>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-blogger"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-instagram"></i>
+            <a href="" class="login100-form-anchor" style="margin-left: 140px"
+              >www.junket.com</a
+            >
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div id="dropDownSelect1"></div>
+    <script src="js/main.js"></script>
+  </body>
+</html>
+
